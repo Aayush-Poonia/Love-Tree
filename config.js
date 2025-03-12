3fee7a289bfd2a381bf9daa568e6eb21b575fc9d
Aayush Poonia <Aayushpoonia10@gmail.com>
2025-03-11 23:39:23 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+
+var config = {
+    // The following sentences do not have to be 7, you can also change it to 4 sentences, 2 sentences are also fine
+    lines: [
+        "Dear Khushi,Khushu,Lalisa,My Everthing,My Jaan",
+        "You Are My Everthing Babe",
+        "I Know We Fights But It Makes Our Bond Strong",
+        "I Will Never Stop Loving You Babe",
+        "You Can Do Whatever You Like",
+        "But I Will Never Give Up On You",
+        "I Will Be With You Forever And By Your Side",
+        "I Will Always Love You My Love!",
+    ],
+    // Time of love Remember to write the format correctly, it is very important
+// The format must be the same as the time format below
+// They are year-month-day T hour: minute: second
+// Warm warning:
+// 1. Hour, minute, and second must be two digits, such as 00:00:00
+// 2. T cannot be lost
+    date: "2022-04-01T08:15:00",
+    // Names of both parties
+    names: ["Khushi", "Aayush"]
+}
